b9df90dc6899c0fc68fbf3656ccf4a4d6bd51a37
manju811943 <manju811943@gmail.com>
2025-12-03 23:19:10 +0530

    intial commits

---
+++ src/test/java/com/new_docker/NewDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.new_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
